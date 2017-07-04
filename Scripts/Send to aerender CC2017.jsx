dbf986fbece28028935d065624d036720b3bf748
Unknown <lennert@any.green>
2017-07-04 18:32:13 +0200

    updated scripts

---
+++ Scripts/Send to aerender CC2017.jsx
@@ -0,0 +1,4 @@
+var filePath = app.project.file.toString();
+var cmd = 'osascript -e \'tell\ application\ "Terminal"\ to\ do\ script\ [\"time\ command\ /Applications/Adobe\\\\ After\\\\ Effects\\\\ CC\\\\ 2017/aerender\ -project '+filePath+'\"]\'';
+system.callSystem(cmd);
+alert("File sent!\nThe current file has been sent to aerender and will now render all active items of your render queue.\nFor more information, check the logs of the Terminal window.");
